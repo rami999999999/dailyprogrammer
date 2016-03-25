f04cd733de2bce8fd2392120c6f9cd8cc727f124
Pedro Ramos <pedrolorgaramos@gmail.com>
2016-03-25 00:41:16 +0000

    some bad update to 257

---
+++ 257word_squares/src/main.rs
@@ -0,0 +1,28 @@
+use std::io::BufReader;
+use std::io;
+use std::env;
+
+struct word{
+text:String,
+n: i32
+}
+
+
+
+fn main() {
+//collecting arguments
+let args: Vec<_> = env::args().collect::<Vec<_>>();
+
+if args.len() < 2 {
+	println!("missing arguments");
+	//exit;
+	}
+
+assert!(args[0].parse::<i32>().is_ok(), "ERROR");
+let size= args[0].parse::<i32>().unwrap();
+if size*size != args[1].chars().collect::<Vec<_>>().len() as i32 {
+        println!("Size of the grid does not match the number of letters")
+        //exit:
+        }
+
+}
