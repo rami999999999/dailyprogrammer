a8886e673c1be1ed400dbb89329b8913d816ff83
Pedro Ramos <pedrolorgaramos@gmail.com>
2016-06-25 19:24:38 +0100

    some changes to 268

--- 268_network_and_cards/server/src/main.rs
+++ 268_network_and_cards/server/src/main.rs
@@ -7,7 +7,30 @@ use clap::{Arg, App, SubCommand};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::string::String;
+use std::sync::mpsc;
+use std::collections::HashMap;
 
+
+#[derive(PartialEq)]
+enum User{
+    username(String),
+    private,
+    None
+}
+
+///function to get e free thread id
+fn get_id( input: &[User]) -> i32 {
+    let mut i=0;
+    let mut found=0;
+    for j in input{
+        if *j != User::None {break;found=1;}
+        else {i+=1;}
+    } 
+    if found == 1 {i}
+    else {-1}
+}
+
+///Executed function when USR is recieved
 fn usr_command(mut stream: &TcpStream, mut lbuffer: &mut [u8], v:bool,id:i32){
     
     if v {println!("thread-{} | executing usr",id)};
@@ -27,7 +50,9 @@ fn usr_command(mut stream: &TcpStream, mut lbuffer: &mut [u8], v:bool,id:i32){
     if v {println!("thread-{} | My username is {}",id,username)}
 }
 
+///function that is started for each recieved connection
 fn handle_client(mut stream: TcpStream, v:bool, id:i32) {
+    
     //short buffer
     let mut buffer: [u8;3] = [0,0,0];
 
@@ -55,10 +80,10 @@ fn handle_client(mut stream: TcpStream, v:bool, id:i32) {
 }
 
 
-
+///Main function of the server
 fn main() {
    
-	 
+    
     let matches = App::new("server for 268 dailyprogramming challange")
 				  .version("1.0")
 				  .author("Pedro Ramos <pedrolorgaramos@gmail.com>")
@@ -78,27 +103,37 @@ fn main() {
                        .help("prints debug information"))
 				  .get_matches();
 
-
+    //verbose flag
     let mut v:bool = false;
-    //starting connection
     let port = matches.value_of("port").unwrap_or("34254");
     let mut id = 0;
- 
+    
+    let  mut p_list : Vec<User> = Vec::with_capacity(500 as usize);
+    for i in 0..500 {
+        p_list.push(User::None);
+    }
+
     let listener = TcpListener::bind(("127.0.0.1", port.parse::<u16>().expect("Port number is invalid"))).expect("Failed to connect");
 
     if matches.is_present("v"){
         v=true;
     }
-    
     // accept connections and process them, spawning a new thread for each one
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                id+=1;
+                
+                //Getting id
+                id = get_id(&p_list);
+                if id == -1 {id = p_list.len() as i32 + 1; p_list.push(User::None);}
+
                 thread::Builder::new().name(format!("thread-{}",id).to_string()).spawn(move|| {
                     if v {println!("starting thread-{}",id)}
                     handle_client(stream, v, id)
                 });
+                //TODO:getusername
+                           
+
             }
             Err(e) => { /* connection failed */ }
         }
